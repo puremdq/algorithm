fd5299015a63120a5ff46d9bafc70901de701168
puremdq <puremdq@gmail.com>
2017-08-27 00:36:40 +0800

    插入排序

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -8,21 +8,17 @@ public class InsertSort {
     private InsertSort() {
     }
 
-    /*带有步长*/
-    public static void insertSort(int[] arr, int... args) {
+    /**
+     * @param arr   int[]  要排序的数组
+     * @param start int  开始位置
+     * @param d     int 步长
+     * @param end   int  结束位置
+     */
+    public static void doInsertSort(int[] arr, int start, int d, int end) {
 
-        int start = 0;
-        int d = 1;
 
-        if (args.length >= 2) {
-
-            start = args[0];
-            d = args[1];
-        }
-
-        int length = arr.length;
         int i, j;
-        for (i = start + d; i < length; i = i + d) {
+        for (i = start + d; i <= end; i = i + d) {
 
             for (j = i; j > start; j = j - d) {
 
@@ -37,23 +33,21 @@ public class InsertSort {
         }
     }
 
-    /*带有步长*/
-    public static void betterInsertSort(int[] arr, int... args) {
 
-        int start = 0;
-        int d = 1;
+    /**
+     * @param arr   int[]  要排序的数组
+     * @param start int  开始位置
+     * @param d     int 步长
+     * @param end   int  结束位置
+     */
+    private static void doBetterInsertSort(int[] arr, int start, int d, int end) {
 
-        if (args.length >= 2) {
-            start = args[0];
-            d = args[1];
-        }
 
-        int length = arr.length;
         int i, tempIndex;
         int temp, flag;
 
         //i之前都已经有序
-        for (i = start + d; i < length; i = i + d) {
+        for (i = start + d; i <= end; i = i + d) {
 
             tempIndex = i;
             flag = 0;
@@ -77,11 +71,30 @@ public class InsertSort {
     }
 
 
-    public static void betterInsertSort(int[] arr) {
-        betterInsertSort(arr, 0, 1);
-    }
+
 
     public static void insertSort(int[] arr) {
-        insertSort(arr, 0, 1);
+        insertSort(arr, 0, 1, arr.length - 1);
     }
+    public static void insertSort(int[] arr, int start, int d) {
+        insertSort(arr, start, d, arr.length - 1);
+    }
+    public static void insertSort(int[] arr, int start, int d, int end) {
+        doInsertSort(arr, start, d, end);
+    }
+
+
+    public static void betterInsertSort(int[] arr) {
+        betterInsertSort(arr, 0, 1, arr.length - 1);
+    }
+    public static void betterInsertSort(int[] arr, int start, int d) {
+        doBetterInsertSort(arr, start, d, arr.length - 1);
+    }
+    public static void betterInsertSort(int[] arr, int start, int d, int end) {
+        doBetterInsertSort(arr, start, d, end);
+    }
+
+
+
+
 }
